6252aaea67da09d1170ad6fc26ae1b97817dd7bf
Lucas <lleao8182@gmail.com>
2024-07-06 19:35:52 -0300

    added some installeverything.sh and fastfetch config

---
+++ installeverything.sh
@@ -0,0 +1,13 @@
+sudo pacman -Sy hyprland nvim firefox discord zsh waybar swaync rofi-wayland kitty hypridle obs-studio
+yay -S swaylock-effects
+
+sudo chmod +x ./lock && cp ./lock ~/.local/bin/lock
+sudo chmod +x ./bateria-fracacpp && mv ./bateria-fracacpp ~
+
+mkdir wallpaper
+cp ./wallhaven-p9p6gj_1920x1080.png ~/Wallpapers
+
+cp ./.zshenv ~
+cp ./kitty ./swaync ./zsh ./waybar ./kitty ./rofi ./hypr ~/.config/
+cp ./icons ~/.local/share/icons/
+
