c6a20bdc6bb7985782f7bdf0659ce915fed58bd8
wykret <lleao8182@gmail.com>
2024-09-29 12:58:28 -0300

    bin

--- installeverything.sh
+++ installeverything.sh
@@ -1,6 +1,7 @@
 sudo pacman -Sy wl-clipboard grim slurp hyprland neovim firefox discord zsh waybar swaync rofi-wayland kitty hypridle obs-studio
 yay -S swaylock-effects
 
+mkdir ~/.local/bin
 sudo chmod +x ./lock && cp ./lock ~/.local/bin/lock
 sudo chmod +x ./bateria-fracacpp && mv ./bateria-fracacpp ~
 
